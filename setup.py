3918d89d43500d5f7d8fea26aee09ef30e5096a8
Andrew Ernest <anernest@usa.net>
2021-08-14 09:45:37 -0500

    Renamed to peoplewiz

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python 
 from setuptools import setup, find_packages 
 
-setup(name='people', version='0.0.1', packages=find_packages())
+setup(name='peoplewiz', version='0.0.1', packages=find_packages())
